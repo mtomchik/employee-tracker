5ea3586ed3529d3a75c0988568a145f247a3dedc
Mtomchik <matthew.tomchik@yahoo.com>
2022-11-06 20:31:15 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // MySQL username,
+    user: 'root',
+    // TODO: Add MySQL password here
+    password: '',
+    database: 'employee_db'
+  },
+  console.log(`Connected to the employee_db database.`)
+);
+
+app.use((req, res) => {
+    res.status(404).end();
+  });
+  
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+  
